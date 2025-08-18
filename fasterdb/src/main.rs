b1cfa658c9ef1f52e0307d12f2be99550392b673
yair Eshel <yair@polywizz.co.il>
2025-08-18 10:11:21 +0300

    dlete update and tests

--- fasterdb/src/main.rs
+++ fasterdb/src/main.rs
@@ -6,22 +6,17 @@ enum OperationError {
     
 }
 #[derive(Debug)]
-struct DataBase {
+struct Collection {
     documents: HashMap<u64, String>,
     next_id: u64
 }
 
-impl DataBase {
-    fn from_hashmap(documents: HashMap<u64, String>) -> Self {
-        let binding     = documents.clone();
-        let next_val = binding.keys().into_iter().max().unwrap_or(&1);
-        Self {
-            documents: documents,
-            next_id: *next_val
-        }
-    }
+impl Collection {
     fn new() -> Self{
-        Self::from_hashmap(HashMap::new())
+        Self {
+            documents: HashMap::new(),
+            next_id: 1
+        }
     }
 
     fn write(&mut self, value: String) -> Result<u64, OperationError> {
@@ -44,36 +39,110 @@ impl DataBase {
         }
     }
 
+    fn delete(&mut self, key: u64) -> Result<String, OperationError> {
+        let value_wrapped = self.documents.remove(&key); 
+        match value_wrapped {
+            Some(value) => {Ok(value)},
+            None => Err(OperationError::KeyMissing)
+        }
+    }
+
 }
 fn main() {
-    let mut database = DataBase::new();
-    println!("{:?}", database);
-    if let Ok(id) =  database.write(String::from("{\"hello\": \"world\"}")) {
-        println!("{:?}", id);
+
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use crate::{Collection, OperationError};
+
+    #[allow(unused_must_use)]
+    fn setup_db() -> Collection {
+        let mut collection = Collection::new();
+        collection.write(String::from("Hello123"));
+        collection
+    }
+    #[test]
+    fn test_create_empty() {
+        let collection = Collection::new();
+        assert_eq!(collection.documents.len(), 0);
+        assert_eq!(collection.next_id, 1);
+    }
+    
+    #[test]
+    fn test_create_entry() {
+        let mut collection = Collection::new();
+        let num = collection.write(String:: from("Hello123"));
+        assert_eq!(num.unwrap(), 1);
+        assert_eq!(collection.documents.len(), 1);
+        assert_eq!(collection.documents.get(&1), Some(&String::from("Hello123")));
+        assert_eq!(collection.next_id, 2);
     }
 
-    for testkey in 1..=2 {
-        let out = database.read(testkey);
-        match out {
-            Ok(Some(val)) => {println!("{}", &val);},
-            Ok(None) => {println!("Found noting!!");},
-            Err(_) => {println!("Something terrible just happen")}
+    #[test]
+    fn test_update_entry() {
+        let mut collection = setup_db();
+        let old_len = collection.documents.len();
+        let update_to = String::from("Hello95");
+        let num_wrapper = collection.update(1, &update_to);
+
+        assert!(num_wrapper.is_ok());
+        if let Ok(num) = num_wrapper {
+            assert_eq!(collection.documents.len(), old_len);
+            assert_eq!(collection.documents.get(&num), Some(&update_to));   
         }
     }
 
-    for testkey in 1..=2 {
-        let out = database.update(testkey, &String::from("{\"Bye\":\"City\"}"));
-        match out {
-            Ok(val) => {
-                println!("Updated {}", &val);
-                if let Ok(Some(new_value)) = database.read(testkey) {
+    #[test]
+    fn test_delete_entry() {
+        let mut collection = setup_db();
+        let old_len = collection.documents.len();
 
-                    println!("{}", new_value)
-                }else{
-                    println!("Why are we here? This should never happen!!!")
-                }
-            },
-            Err(e) => {println!("Got error {:?}", e)}
-        }
+        let pre_del_result = collection.read(1);
+
+        assert!(pre_del_result.is_ok());
+        let pre_del_option = pre_del_result.unwrap();
+
+        assert!(pre_del_option.is_some());
+        let pre_del = pre_del_option.unwrap().clone();
+
+        let deleted_string_result = collection.delete(1);
+
+        assert!(deleted_string_result.is_ok());
+        let deleted_string = deleted_string_result.unwrap();
+
+        assert_eq!(collection.documents.len(), old_len - 1);
+        assert_eq!(pre_del, deleted_string);
     }
+
+    #[test]
+    fn test_read_unexisting() {
+        let collection = setup_db();
+
+        let res = collection.read(12);
+        assert!(res.is_ok()); // Note: missing entry is not an error
+
+        let opt = res.unwrap();
+        assert!(opt.is_none());
+    }
+
+    #[test]
+    fn test_delete_unexisting() {
+        let mut collection = setup_db();
+        
+        let result = collection.delete(99);
+        assert!(matches!(result, Err(OperationError::KeyMissing)))
+    }
+    
+    #[test]
+    fn test_update_unexisting() {
+        let mut collection = setup_db();
+        
+        let result = collection.update(99, &String::from("Yo yo yo"));
+        assert!(matches!(result, Err(OperationError::KeyMissing)))
+    }
+
+
 }
